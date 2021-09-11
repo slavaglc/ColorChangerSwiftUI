5724581527f48a135d7b80683932b988e2d00536
slavaglc <47712523+slavaglc@users.noreply.github.com>
2021-09-11 10:40:07 +0300

    Initial Commit

---
+++ ColorChanger/ColorChangerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorChangerApp.swift
+//  ColorChanger
+//
+//  Created by slava on 11.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct ColorChangerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
